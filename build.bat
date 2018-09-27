094c24f8905bac7ddc7f6c92f7ed0740322c7d48
David Newcomb <david.newcomb@bigsoft.co.uk>
2018-09-27 23:23:50 +0100

    Update builders

--- build.bat
+++ build.bat
@@ -1,2 +1,12 @@
 rem Prepare for release
-.\node_modules\uglify-js\bin\uglifyjs jquery.imgbox.js -m -c > jquery.imgbox.min.js
+
+echo "/*"                                                             > jquery.imgbox.min.js
+echo " *  jquery-imgbox - v1.1.0"                                     >> jquery.imgbox.min.js
+echo " *  A jQuery plugin that draws a box over an image."            >> jquery.imgbox.min.js
+echo " *  https://github.com/davidnewcomb/jquery-imgbox/"             >> jquery.imgbox.min.js
+echo " *"                                                             >> jquery.imgbox.min.js
+echo " *  Copyright (c) 2018 David Newcomb, http://www.bigsoft.co.uk" >> jquery.imgbox.min.js
+echo " *  MIT License"                                                >> jquery.imgbox.min.js
+echo " */"                                                            >> jquery.imgbox.min.js
+
+.\node_modules\uglify-js\bin\uglifyjs jquery.imgbox.js -m -c >> jquery.imgbox.min.js
