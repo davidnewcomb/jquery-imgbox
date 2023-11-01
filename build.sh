ccfbe3dfae47f712574cbd544ffe5a74d91ec96d
David Newcomb <david.newcomb@bigsoft.co.uk>
2023-11-01 18:48:43 +0000

    Build file

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yarn install
+cat << EOF > jquery.imgbox.min.js
+/*
+ *  jquery-imgbox - v1.2.4
+ *  A jQuery plugin that draws a box over an image.
+ *  https://github.com/davidnewcomb/jquery-imgbox/
+ *
+ *  Copyright (c) 2018 David Newcomb, http://www.bigsoft.co.uk
+ *  MIT License
+ */
+EOF
+./node_modules/uglify-js/bin/uglifyjs jquery.imgbox.js -m -c >> jquery.imgbox.min.js
